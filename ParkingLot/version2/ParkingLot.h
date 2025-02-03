bfca14e16bddb8715d3fe7295b9c0a037a869322
tarun953 <tarunmisr953@gmail.com>
2025-02-03 12:26:59 +0530

    Changed small things in ParkingLot.h

--- ParkingLot/version2/ParkingLot.h
+++ ParkingLot/version2/ParkingLot.h
@@ -1,4 +1,5 @@
 #include <C:\Users\HalfVampire\Documents\LLD\ParkingLot\version2\Floor.h>
+// #include <./Floor.h>
 #include <vector>
 #include <string>
 #include <map>
@@ -12,6 +13,16 @@ private:
     std::vector<Floor> floors_;
 
 public:
-    ParkingLot(std::string _parkingId) {};
+    // I am expecting the parking lot capacity vector stores the capacity of individual floors with their individual capacities
+    ParkingLot(std::string _parkingId, std::string _parkingName, std::string _address, int _numFloors, std::vector<std::vector<int>> _floorCapacities) : parkingId_(_parkingId), parkingName_(_parkingName), address_(_address)
+    {
+        floors_.clear();
+        for (int i = 0; i < _numFloors; i++)
+        {
+            std::vector<int> _tempCapacity = _floorCapacities[i];
+            // Call the Floor constructor here
+            // floors_.push_back(Floor())
+        }
+    };
     ~ParkingLot();
 };
